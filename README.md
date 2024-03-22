1020c9493a7094625c21fa700b9779ed2145799a
MaiQD <q.dat2710@gmail.com>
2024-03-23 00:09:26 +0700

    Update readme and add batch-install file

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # chocolatey-install-script
 Install programs using chocolatey
+## Install Chocolatey
+Install Url: https://chocolatey.org/install#individual
+
+Running in <b>Administrator</b> Powershell
+
+## Install Git
+
+## Running installation scrip
+Navigate git folder 
+Open Powershell as Adminstrator 
+Run command 
+```sh
+./batch-install.ps1
+```
+## Programes included in shell script
+- googlechrome 
+- nvm.portable 
+- notepadplusplus 
+- winrar 
+- vscode 
+- azure-cli 
+- docker-desktop 
+- dbeaver 
+- obs-studio 
+- drawio 
+- mobaxterm 
+- lightshot 
+- notion 
---
+++ batch-install.ps1
@@ -0,0 +1,18 @@
+choco install googlechrome -y
+choco install nvm.portable -y
+choco install notepadplusplus -y
+choco install winrar -y
+choco install vscode -y
+choco install git.install --params "'/Editor:VisualStudioCode /WindowsTerminalProfile'" -y
+choco install azure-cli -y
+choco install docker-desktop -y
+choco install dbeaver -y
+choco install obs-studio -y
+choco install drawio -y
+choco install mobaxterm -y
+choco install lightshot -y
+choco install notion -y
+choco install linqpad -y
+#choco install beyondcompare -y
+echo("Refreshing Environment variables")
+refreshenv
