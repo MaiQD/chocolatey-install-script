639c0b720337f7a2371c75e513cc56bdeef4e109
Mai Quốc Đạt <38066615+MaiQD@users.noreply.github.com>
2024-07-03 15:27:08 +0700

    Update batch-install.ps1

--- batch-install.ps1
+++ batch-install.ps1
@@ -1,6 +1,27 @@
+param([bool]$installChoco=0)
+
+#Install Chocolatey
+if($installChoco -eq 1)
+{
+    Write-Host "Installing Chocolatey...";
+
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    
+    Write-Host "Finish install Chocolatey...";
+}
+else
+{
+    Write-Host "Omit the installation of Chocolatey";
+}
+
+$chocoVersion = choco --version
+Write-Host "Using Chocolatey version: $chocoVersion"
+
+#Install packages
 choco install googlechrome -y
 choco install nvm.portable -y
 choco install notepadplusplus -y
+choco install foxitreader -y
 choco install winrar -y
 choco install vscode -y
 choco install azure-cli -y
@@ -17,6 +38,6 @@ choco install postman -y
 wsl --install
 choco install docker-desktop -y
 choco install git.install --params "'/Editor:VisualStudioCode /WindowsTerminalProfile'" -y
-#choco install beyondcompare -y
+choco install beyondcompare -y
 echo("Refreshing Environment variables")
 refreshenv
